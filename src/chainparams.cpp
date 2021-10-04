7fa48c2e3ab0ee6c23758b5a7015ea6c22922ef1
mymuzk <35181597+mymuzk@users.noreply.github.com>
2021-10-04 23:53:05 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -113,10 +113,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0xf1;
+        pchMessageStart[1] = 0xcb;
+        pchMessageStart[2] = 0xb0;
+        pchMessageStart[3] = 0xdc;
         nDefaultPort = 9333;
         nPruneAfterHeight = 100000;
 
@@ -218,10 +218,10 @@ public:
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x1efb29c8187d5a496a33377941d1df415169c3ce5d8c05d055f25b683ec3f9a3"); //612653
 
-        pchMessageStart[0] = 0xfd;
+        pchMessageStart[0] = 0xf1;
         pchMessageStart[1] = 0xd2;
-        pchMessageStart[2] = 0xc8;
-        pchMessageStart[3] = 0xf1;
+        pchMessageStart[2] = 0xc3;
+        pchMessageStart[3] = 0xf2;
         nDefaultPort = 19335;
         nPruneAfterHeight = 1000;
 
@@ -304,10 +304,10 @@ public:
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x00");
 
-        pchMessageStart[0] = 0xfa;
-        pchMessageStart[1] = 0xbf;
-        pchMessageStart[2] = 0xb5;
-        pchMessageStart[3] = 0xda;
+        pchMessageStart[0] = 0xf1;
+        pchMessageStart[1] = 0xb3;
+        pchMessageStart[2] = 0xc5;
+        pchMessageStart[3] = 0xd2;
         nDefaultPort = 19444;
         nPruneAfterHeight = 1000;
 
